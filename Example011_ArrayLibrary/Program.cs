3885c22e6e201f2318fe5d70e42b4f496c2bd39c
Tatyana <tatyana.kurii@mail.ru>
2023-01-26 14:07:19 +0600

    Example 011 Array library

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -21,7 +21,28 @@ void PrintArray(int[] col)
   }
 }
 
+int IndexOf(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = 0;
+
+  while(index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+    }
+  }
+  return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+
+Console.WriteLine(pos);
